91a4d9fc8e3b36e5d2b5c67b2cdce5ef66673e19
kvl38 <pat38ru@gmail.com>
2020-09-18 11:15:24 +0300

    4

---
+++ lesson4/lesson4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace prak4_02_upr02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Stack stack = new Stack();
+      stack.Push("1-st");
+      stack.Push("2-nd");
+      stack.Push("3-d");
+      stack.Push("Go");
+      while (stack.Count > 0)
+      {
+        object obj = stack.Pop();
+        Console.WriteLine("'From Stack: {0}", obj);
+      }
+
+    }
+  }
+}
