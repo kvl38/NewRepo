53230763485e331109ffaa56285f5c564284df55
kvl38 <pat38ru@gmail.com>
2020-09-18 10:59:03 +0300

    2

---
+++ LES20/LES20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] text ={"Dan",
+        "C++",
+        "FFF44443332DD",
+        " +79263772622",
+        " 892667777777",
+        " (956)9090905" };
+
+      foreach (string item in text)
+      {
+        string pattern = @"\d{4}";
+        if (Regex.IsMatch(item, pattern))
+        {
+          //Console.WriteLine($"{item} это телефон"); 
+          Console.WriteLine(item + " это телефон");
+        }
+        else
+        {
+          Console.WriteLine(
+          string.Format("{0} это НЕ телефон", item)
+          );
+        }
+
+      }
+
+    }
+  }
+}
